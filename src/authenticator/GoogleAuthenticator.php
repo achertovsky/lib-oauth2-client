65d6ff78bc1be9de96a4902ed21adc0f8c1d99ff
Alexander Zaiets <alexzaets.dev@gmail.com>
2023-05-13 17:14:04 +0300

    main logic built

--- src/authenticator/GoogleAuthenticator.php
+++ src/authenticator/GoogleAuthenticator.php
@@ -7,19 +7,115 @@ namespace achertovsky\oauth\authenticator;
 use achertovsky\oauth\entity\UserData;
 use Psr\Http\Client\ClientInterface;
 use achertovsky\oauth\entity\Request;
+use achertovsky\oauth\exception\EmailNotVerifiedException;
+use achertovsky\oauth\exception\OauthException;
+use achertovsky\oauth\exception\WrongOauthScopeException;
+use Psr\Http\Client\ClientExceptionInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use RuntimeException;
 
 class GoogleAuthenticator implements AuthenticatorInterface
 {
+    private const EMAIL = 'email';
+    private const EMAIL_VERIFIED = 'email_verified';
+
     public function __construct(
-        private ClientInterface $client
+        private ClientInterface $client,
+        private RequestBuilderInterface $requestBuilder,
+        private string $authenticateUrl,
+        private string $clientId,
+        private string $clientSecret,
+        private string $redirectUrl
     ) {
     }
 
+    /**
+     * @throws ClientExceptionInterface
+     * @throws OauthException
+     * @throws WrongOauthScopeException
+     * @throws EmailNotVerifiedException
+     */
     public function authenticate(string $code): UserData
     {
-        $request = new Request();
-        $this->client->sendRequest($request);
+        $payload = $this->fetchPayload($code);
+        $this->validatePayload($payload);
 
-        return new UserData('no');
+        return new UserData($payload[self::EMAIL]);
+    }
+
+    private function fetchPayload(string $code): array
+    {
+        $jwt = $this->fetchIdTokenData($code);
+
+        $jwtParts = explode('.', $jwt);
+        if (!array_key_exists(1, $jwtParts)) {
+            throw new OauthException('Invalid jwt');
+        }
+
+        $payload = json_decode(base64_decode($jwtParts[1]), true);
+        if (!is_array($payload)) {
+            throw new OauthException('Invalid payload');
+        }
+
+        return $payload;
+    }
+
+    private function validatePayload(array $payload): void
+    {
+        if (!array_key_exists(self::EMAIL, $payload)) {
+            throw new WrongOauthScopeException('Missing email scope');
+        }
+
+        if (
+            !array_key_exists(self::EMAIL_VERIFIED, $payload)
+            || $payload[self::EMAIL_VERIFIED] !== true
+        ) {
+            throw new EmailNotVerifiedException('Unverified email received');
+        }
+    }
+
+    private function fetchIdTokenData(string $code): string
+    {
+        $tokenData = json_decode(
+            $this->fetchContent(
+                $this->prepareRequest($code)
+            ),
+            true
+        );
+        if ($tokenData === null) {
+            throw new OauthException('Not a json data');
+        }
+
+        if (!array_key_exists('id_token', $tokenData)) {
+            throw new WrongOauthScopeException('Expected payload not received');
+        }
+
+        return $tokenData['id_token'];
+    }
+
+    private function prepareRequest(string $code): RequestInterface
+    {
+        return $this->requestBuilder->buildRequest(
+            $this->authenticateUrl,
+            [
+                'grant_type' => 'authorization_code',
+                'client_id' => $this->clientId,
+                'client_secret' => $this->clientSecret,
+                'redirect_uri' => $this->redirectUrl,
+                'code' => $code,
+            ],
+            'POST'
+        );
+    }
+
+    private function fetchContent(RequestInterface $request): string
+    {
+        $response = $this->client->sendRequest($request);
+        try {
+            return $response->getBody()->getContents();
+        } catch (RuntimeException $exception) {
+            throw new OauthException('Failed to fetch token');
+        }
     }
 }
--- tests/unit/GoogleAuthenticatorTest.php
+++ tests/unit/GoogleAuthenticatorTest.php
@@ -4,35 +4,160 @@ declare(strict_types=1);
 
 namespace achertovsky\tests\unit;
 
-use PHPUnit\Framework\MockObject\MockObject;
-use Psr\Http\Client\ClientInterface;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\StreamInterface;
+use achertovsky\oauth\entity\UserData;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+use achertovsky\oauth\exception\OauthException;
 use achertovsky\oauth\authenticator\GoogleAuthenticator;
-use achertovsky\oauth\entity\Request;
+use achertovsky\oauth\exception\WrongOauthScopeException;
+use achertovsky\oauth\authenticator\RequestBuilderInterface;
+use achertovsky\oauth\exception\EmailNotVerifiedException;
+use RuntimeException;
 
 class GoogleAuthenticatorTest extends TestCase
 {
     private const AUTH_CODE = 'code';
+    private const URI = 'http://oauth-google.com/';
+    private const EXPECTED_EMAIL = 'email@gmail.com';
+    private const CLIENT_ID = 'appid';
+    private const CLIENT_SECRET = 'appsecret';
+    private const REDIRECT_URL = 'http://baseurl.com';
 
-    private MockObject $client;
+    private MockObject $clientMock;
+    private MockObject $builderMock;
 
     private GoogleAuthenticator $authenticator;
 
     protected function setUp(): void
     {
-        $this->client = $this->createMock(ClientInterface::class);
-        $this->authenticator = new GoogleAuthenticator($this->client);
+        $this->clientMock = $this->createMock(ClientInterface::class);
+        $this->builderMock = $this->createMock(RequestBuilderInterface::class);
+
+        $this->authenticator = new GoogleAuthenticator(
+            $this->clientMock,
+            $this->builderMock,
+            self::URI,
+            self::CLIENT_ID,
+            self::CLIENT_SECRET,
+            self::REDIRECT_URL
+        );
     }
 
-    public function testClientIsCalled(): void
+    public function testAuthenticate(): void
     {
-        $this->client
+        $this->configureMocks(__DIR__ . '/../fixture/google_response.json');
+        $this->assertEquals(
+            new UserData(
+                self::EXPECTED_EMAIL
+            ),
+            $this->authenticator->authenticate(
+                self::AUTH_CODE
+            )
+        );
+    }
+
+    public static function dataIssueAuthenticate(): array
+    {
+        return [
+            'id_token is not jwt' => [
+                __DIR__ . '/../fixture/google_response_id_token_not_jwt.json',
+                OauthException::class,
+            ],
+            'id_token missing' => [
+                __DIR__ . '/../fixture/google_response_id_token_not_set.json',
+                WrongOauthScopeException::class,
+            ],
+            'no email' => [
+                __DIR__ . '/../fixture/google_response_no_email.json',
+                WrongOauthScopeException::class,
+            ],
+            'email not verified' => [
+                __DIR__ . '/../fixture/google_response_unverified_email.json',
+                EmailNotVerifiedException::class,
+            ],
+            'response not json' => [
+                __DIR__ . '/../fixture/google_response_not_json',
+                OauthException::class,
+            ],
+            'jwt malformed' => [
+                __DIR__ . '/../fixture/google_response_id_token_jwt_malformed.json',
+                OauthException::class,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataIssueAuthenticate
+     */
+    public function testIssueAuthenticate(
+        string $fileToRead,
+        string $expectedException
+    ): void {
+        $this->expectException($expectedException);
+        $this->configureMocks($fileToRead);
+        $this->authenticator->authenticate(self::AUTH_CODE);
+    }
+
+    public function testIssueAuthenticateStreamContentsIssue(): void
+    {
+        $this->expectException(OauthException::class);
+        $this->configureMocks();
+        $this->authenticator->authenticate(self::AUTH_CODE);
+    }
+
+    private function configureMocks(
+        ?string $fileName = null
+    ): void {
+        $requestMock = $this->createMock(RequestInterface::class);
+
+        $this->builderMock
             ->expects($this->once())
-            ->method('sendRequest')
+            ->method('buildRequest')
+            ->with(
+                self::URI,
+                [
+                    'grant_type' => 'authorization_code',
+                    'client_id' => self::CLIENT_ID,
+                    'client_secret' => self::CLIENT_SECRET,
+                    'redirect_uri' => self::REDIRECT_URL,
+                    'code' => self::AUTH_CODE
+                ]
+            )
+            ->willReturn($requestMock)
         ;
 
-        $this->authenticator->authenticate(
-            self::AUTH_CODE
-        );
+        $streamMock = $this->createMock(StreamInterface::class);
+        if ($fileName !== null) {
+            $streamMock
+                ->method('getContents')
+                ->willReturn(
+                    file_get_contents($fileName)
+                )
+            ;
+        } else {
+            $streamMock
+                ->method('getContents')
+                ->willThrowException(
+                    new RuntimeException()
+                )
+            ;
+        }
+
+        $responseMock = $this->createMock(ResponseInterface::class);
+        $responseMock
+            ->method('getBody')
+            ->willReturn($streamMock)
+        ;
+
+        $this->clientMock
+            ->expects($this->once())
+            ->method('sendRequest')
+            ->with($requestMock)
+            ->willReturn($responseMock)
+        ;
     }
 }
